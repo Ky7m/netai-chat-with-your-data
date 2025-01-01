2c55842549c18fa1a84e2e0dd4c4f484c7fcb096
Bruno Capuano <bcapuano@gmail.com>
2025-01-01 15:37:36 -0500

    Add messaging form and restructure layout components
    - Introduced a new form for sending messages in `Chat.razor`, including a textarea and submit button, with a line separator added.
    - Removed background colors from `.chat` and `.write-message` in `Chat.razor.css`.
    - Updated modal title in `ChatMessage.razor` and changed user message timestamp to use `ChatMessageDateTime`.
    - Restructured `MainLayout.razor` to utilize `BlazorBootstrapLayout`, adding sidebar and content sections with dynamic navigation items.
    - Added `ChatMessageDateTime` property in `ChatWithDocuments.cs` to track message timestamps.

--- src/ChatWYData.ChatApp/Model/ChatWithDocuments.cs
+++ src/ChatWYData.ChatApp/Model/ChatWithDocuments.cs
@@ -6,6 +6,7 @@ namespace ChatWYData.ChatApp.Model;
 public class ChatMessageWithDocuments
 {
     public ChatMessage ChatMessage { get; set; } = null;
+    public DateTime ChatMessageDateTime { get; set; } = DateTime.Now;
     public List<Document> Documents { get; set; } = new();
 
     public void Add(UserChatMessage incomingUserMessage)
